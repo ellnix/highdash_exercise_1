9af68a5491b8bbf91edc3e7ddf7374db292ae32d
ellnix <ellnix@disroot.org>
2024-08-29 16:28:17 +0200

    Mark tests as optional
    Previously these tests would have detracted from the value of the lesson.
    This way the focus stays on the main logic of the functions rather than edgecase
    error-handling.

--- test/sum.js
+++ test/sum.js
@@ -2,19 +2,8 @@ import highdash from '../highdash.js'
 import assert from 'assert'
 
 describe('Highdash.sum', () => {
-  it('returns 0 when passed null or undefined', () => {
-    assert.equal(highdash.sum(null), 0)
-    assert.equal(highdash.sum(undefined), 0)
-  })
-
   it('returns 0 when passed empty array', () => {
     assert.equal(highdash.sum([]), 0)
-  })
-
-  it('throws an error if passed non-array', () => {
-    assert.throws(() => highdash.sum(5), Error, 'Argument is not an array!')
-    assert.throws(() => highdash.sum("hello world"), Error, 'Argument is not an array!')
-    assert.throws(() => highdash.sum({hello: "world"}), Error, 'Argument is not an array!')
   })
 
   it('sums all elements', () => {
@@ -22,4 +11,17 @@ describe('Highdash.sum', () => {
     assert.equal(highdash.sum([45, 18, 51, 48, 96]), 258)
     assert.equal(highdash.sum([97, 80, 64, 23, 77, 11, 24, 55, 79, 34]), 544)
   })
+
+  // optional tests, complete for extra credit:
+  //
+  // it('returns 0 when passed null or undefined', () => {
+  //   assert.equal(highdash.sum(null), 0)
+  //   assert.equal(highdash.sum(undefined), 0)
+  // })
+
+  // it('throws an error if passed non-array', () => {
+  //   assert.throws(() => highdash.sum(5), Error, 'Argument is not an array!')
+  //   assert.throws(() => highdash.sum("hello world"), Error, 'Argument is not an array!')
+  //   assert.throws(() => highdash.sum({hello: "world"}), Error, 'Argument is not an array!')
+  // })
 })
